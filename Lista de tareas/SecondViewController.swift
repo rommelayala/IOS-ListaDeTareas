e13dd13f1b2900237f20a40e4356a6c688ba80e1
rommelayala <rommelsuscripciones@gmail.com>
2016-03-26 17:58:38 +0100

    nueva funcionalidad de borrar elementos de la lista

--- Lista de tareas/SecondViewController.swift
+++ Lista de tareas/SecondViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
 
+class SecondViewController: UIViewController,UITextFieldDelegate {
+
+    @IBOutlet weak var addTarea: UIButton!
+    @IBOutlet weak var addTareaText: UITextField!
+    
+    
+    @IBAction func addTareaBoton(sender: AnyObject) {
+        toDoList.append(addTareaText.text!)
+        addTareaText.text = ""
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        addTareaText.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+//esconder teclado
+    
+    //funcion hace desaparecer el teclado cuando el textbox pierde el foco
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
 
 }
 
